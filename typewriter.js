07f37d27bb9bcf12abd48c4c4a6e336dc889104f
Amino Asmaa Ali <amino.asmaa.ali@gmail.com>
2020-02-11 17:52:21 +0000

    how to create animated sentences using timout

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+let sentence = "hello there from lighthouse labs";
+let waitTime = 500;
+const printString = () => {
+  sentence += '\n'
+  for (const char of sentence) {
+    setTimeout( () => {
+      process.stdout.write(char);
+    }, waitTime)
+    waitTime += 100
+  }
+}
+  printString();
